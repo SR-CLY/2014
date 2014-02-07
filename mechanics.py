167f10f5f0190bcb6efee5a67c8d318cc10b78a1
Nikita <nikita.karandej@gmail.com>
2014-02-07 16:28:30 +0000

    upd

--- mechanics.py
+++ mechanics.py
@@ -1,8 +1,10 @@
 from time import clock, sleep
 from math import copysign
 
-NOTHES_PER_WHEEL = 9
-WHEEL_SEPARATION = 0.37
+from sr import INPUT_PULLUP
+
+NOTHES_PER_WHEEL = 4
+ROBOT_RADIUS = 0.185
 M_SWITCH_LEFT  = 12
 M_SWITCH_RIGHT = 2
 WHEEL_CIRCUMFERENCE = 0.31
@@ -14,6 +16,7 @@ class Motor:
         self.k = k
         self.switchID = switchID
         self.ruggeduino = rduino
+        self.ruggeduino.pin_mode(switchID, INPUT_PULLUP)
 
         self.turnsMade = 0
         self.notchesPassed = 0
@@ -26,7 +29,10 @@ class Motor:
         start = clock()
         while self.ruggeduino.digital_read(self.switchID) is False:
             pass
-        return clock() - start
+        dt = clock() - start
+        print 'Time for a notch:%.4f' % (dt)
+        return dt
+        # return clock() - start
 
     def count_notches(self):
         self.timeActive += self.time_a_switch()
@@ -51,29 +57,31 @@ class Journey:
         if distance != 0:
             self.length = distance
         elif angle != 0:
-            self.length = angle * WHEEL_SEPARATION / 2
+            self.length = angle * ROBOT_RADIUS
 
-# Better use notches here.
+        # Better use notches here.
         self.turnsToDo = abs(self.length // WHEEL_CIRCUMFERENCE)
         direction1 = copysign(1, self.length)
         direction2 = direction1
         if angle != 0:
             direction2 *= -1
       
-        self.m1 = Motor(robot.motors[0].m0, M_SWITCH_LEFT, rduino, direction1)
-        self.m2 = Motor(robot.motors[0].m1, M_SWITCH_RIGHT, rduino, direction2)
+        self.m0 = Motor(robot.motors[0].m0, M_SWITCH_LEFT, rduino, direction1)
+        self.m1 = Motor(robot.motors[0].m1, M_SWITCH_RIGHT, rduino, direction2)
 
     def start(self):
-        for each in [self.m1, self.m2]: each.start()
+        for each in [self.m0, self.m1]: each.start()
 
         while (self.m1.notchesPassed // NOTHES_PER_WHEEL) < self.turnsToDo:
+            a = clock()
+            self.m0.count_notches()
             self.m1.count_notches()
-            self.m2.count_notches()
+            print 'Time delay due to timing (loop): %.4f' % (clock() - a)
             self.sync_power()
-            sleep(0.05)  # Sync every 0.05 seconds?
+            sleep(2)  # Sync every 0.05 seconds?
 
+        self.m0.stop()
         self.m1.stop()
-        self.m2.stop()
 
     def sync_power(self):
         # Increase/Decrease power.
@@ -81,6 +89,11 @@ class Journey:
         # Number of significant figures is important here
         # Need to work out number of significant figures to be used.
         # 10 is just very rough estimate and should be properly worked out.
-        base_notch_dt = self.m1.time_a_switch()
-        notch_dt = self.m2.time_a_switch()
-        self.m2.set_power(self.m2.power + (notch_dt-base_notch_dt)*10*self.m2.k)
+        base_notch_dt = self.m0.time_a_switch()
+        notch_dt = self.m1.time_a_switch()
+
+        dt = notch_dt - base_notch_dt
+        print 'difference between notch_dt and base_notch_dt %.4f' % (dt)
+        self.m1.set_power(self.m1.power + dt*1*self.m1.k)
+
+        # m1.set_power(m1.power + (notch_dt-base_notch_dt)*10*m1.k)
