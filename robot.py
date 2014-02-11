cb5275610affb9960bf76d253cbf66c6627a7ef7
Jake Howard <git@theorangeone.net>
2014-02-11 15:51:35 +0000

    update from local files

--- robot.py
+++ robot.py
@@ -6,26 +6,27 @@ from sr import Robot
 from movements import move_straight, turn
 from position import compute_directions_for
 
-def main0():
-    robot = Robot()
-    markersInSight = robot.see()
-    while not markersInSight:
-        turn(robot)
-        sleep(0.5)
-        markersInSight = robot.see()
-
-    marker = markersInSight[0]
+def line_up_to(marker):
     dist, angle1, angle2 = compute_directions_for(marker)
-#    print 'dist=%.2f, angle1=%.2f, angle2=%.2f' % (distance, angle1, angle2)
-
+    print 'dist=%.2f, angle1=%.2f, angle2=%.2f' % (distance, angle1, angle2)
     turn(robot, angle1)
     sleep(1)
     move_straight(robot, dist)
     sleep(1)
     turn(robot, angle2)
-
+    
 def main():
+    worldExists = True
     robot = Robot()
-    move_straight(robot, 10)  # 10 meters :D
+    markersInSight = robot.see()
+    while worldExists:
+        while not markersInSight:
+            turn(robot)
+            sleep(0.5)
+            markersInSight = robot.see()
+        line_up_to(markersInSight[0])
+        while robot.see() is not []:
+            pass
+        sleep(5)
 
 main()
