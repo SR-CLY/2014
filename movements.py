57ef5fbf97c19c0930a7abf8e28dacacb8228339
Jake Howard <git@theorangeone.net>
2014-02-11 15:52:12 +0000

    update from local files

--- movements.py
+++ movements.py
@@ -4,9 +4,6 @@ def move_straight(robot, dist):
     journey = Journey(robot, distance=dist)
     journey.start()
 
-def turn(robot, alpha=0.524):
-    """
-    0.524 rad = 30 degrees
-    """
+def turn(robot, alpha=0.524):  # 0.524 rad = 30 degrees
     journey = Journey(robot, angle=alpha)
     journey.start()
