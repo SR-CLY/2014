444df662d49e0d51d66dc9d54e681e87ba92746e
Jake Howard <git@theorangeone.net>
2014-02-12 15:25:14 +0000

    changed debug to 90deg

--- robot.py
+++ robot.py
@@ -21,7 +21,7 @@ def line_up_to(marker, robot):
 def main():
     world_exists = True
     robot = Robot()
-    turn(robot, pi)
+    turn(robot, 0.5*pi)
     return
 
     markers = robot.see(res=res)
