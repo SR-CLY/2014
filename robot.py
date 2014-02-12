516f413e004692e5203b41823428f03fc77c6526
Jake Howard <git@theorangeone.net>
2014-02-12 14:41:08 +0000

    higher res pictures

--- robot.py
+++ robot.py
@@ -6,6 +6,9 @@ from sr import Robot
 from movements import move_straight, turn
 from position import compute_directions_for
 
+res = (1280, 720)
+
+
 def line_up_to(marker, robot):
     dist, angle1, angle2 = compute_directions_for(marker)
     print 'dist=%.2f, angle1=%.2f, angle2=%.2f' % (dist, angle1, angle2)
@@ -18,15 +21,15 @@ def line_up_to(marker, robot):
 def main():
     worldExists = True
     robot = Robot()
-    markersInSight = robot.see()
+    markersInSight = robot.see(res=res)
     while worldExists:
-        markersInSight = robot.see()
+        markersInSight = robot.see(res=res)
         while not markersInSight:
             turn(robot)
             sleep(0.5)
-            markersInSight = robot.see()
+            markersInSight = robot.see(res=res)
         line_up_to(markersInSight[0], robot)
-        while robot.see() is not []:
+        while robot.see(res=res):
             pass
         sleep(5)
 
