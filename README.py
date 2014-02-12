87a9154b34b030ec2607fc4c294d9059c0592054
Nikita <nikita.karandej@gmail.com>
2014-02-12 20:19:57 +0000

    File deletion

--- README.py
+++
@@ -1,4 +0,0 @@
-"""
-Hi, I see you have replaced 'mechanics.py' here with some older file. There have been made some changes to code which are not in the file you have uploaded.
-You can see that version if you go to revision and select the file at 14:35.
-"""
