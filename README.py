79eff7a7d8bf140c84c87ec1b0e87c2ed1865364
Nikita <nikita.karandej@gmail.com>
2014-02-12 15:36:45 +0000

    a message

---
+++ README.py
@@ -0,0 +1,4 @@
+"""
+Hi, I see you have replaced 'mechanics.py' here with some older file. There have been made some changes to code which are not in the file you have uploaded.
+You can see that version if you go to revision and select the file at 14:35.
+"""
