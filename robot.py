b54b70fa90a6cae4be25de69f938dde8f30c7157
olls <olls@ovi.com>
2014-02-26 15:47:47 +0000

    0.2

--- robot.py
+++ robot.py
@@ -6,8 +6,8 @@ from sr import Robot
 from movements import move_straight, turn
 from position import compute_directions_for
 
-def line_up_to(marker, robot):
-    dist, angle1, angle2 = compute_directions_for(marker)
+def line_up_to(marker, robot, dist=0.2):
+    dist, angle1, angle2 = compute_directions_for(marker, d=0.2)
     print 'dist=%.2f, angle1=%.2f, angle2=%.2f' % (dist, angle1, angle2)
     turn(robot, angle1)
     sleep(1)
@@ -24,14 +24,21 @@ def main():
         sleep(0.5)
         markersInSight = robot.see()
     marker = markersInSight[0]
-    # dist0 = marker.dist
-    # move_straight(robot, 1)
-    # sleep(1)
-    # marker = robot.see()[0]
-    # print '%.2f' % (dist0-marker.dist)
     line_up_to(marker, robot)
+    touchingMarker = lambda: robot.ruggeduinos[0].digital_read( 11 )
+    robot.motors[0].m0.power = 30
+    robot.motors[0].m1.power = 30
+    robot.ruggeduinos[0].pin_mode( 11, INPUT_PULLUP )
+    while not touchingMarker():
+        print "Not hit anything yet, moving forward"
+    print'Touching Marker'
+    robot.motors[0].m0.power = 0
+    robot.motors[0].m1.power = 0
 
 robot = Robot()
 while 1:
     main()
     sleep(10)
+
+
+main()
