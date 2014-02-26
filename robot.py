a6f7efcf92bfe89e705e2bfbd73bcab1e19b30b9
Nikita <nikita.karandej@gmail.com>
2014-02-26 15:02:41 +0000

    angles

--- robot.py
+++ robot.py
@@ -19,42 +19,15 @@ def main():
     worldExists = True
     robot = Robot()
     markersInSight = robot.see()
-    while worldExists:
+    while not markersInSight:
+        # turn(robot)
+        # sleep(0.5)
         markersInSight = robot.see()
-        while not markersInSight:
-            turn(robot)
-            sleep(0.5)
-            markersInSight = robot.see()
+    marker = markersInSight[0]
+    angle = marker.orientation.rot_y
+    turn(robot, angle)
+    # line_up_to(markersInSight[0], robot)
 
-        line_up_to(markersInSight[0], robot)
-        sleep(5)
-
-def turning_test():
-    robot = Robot()
-    testNumber = 1
-    if testNumber == 0:
-        angles = [pi/2, pi/2, pi/4, pi/4]
-        for a in angles:
-            turn(robot, a)
-            sleep(5)
-    else:
-        markersInSight = robot.see()
-        while not markersInSight:
-            markersInSight = robot.see()
-        angles = [pi/6, pi/4]
-        for a in angles:
-            for i in range(2):
-                angle0 = markersInSight[0].orientation.rot_y
-                turn(robot, a)
-                sleep(1)
-                try:
-                    print 'Turned %.1f radians, tried to turn %.1f radians'\
-                        % (robot.see()[0].orientation.rot_y - angle0, a)
-                except:
-                    print 'Marker lost'
-                sleep(10)
-
-robot = Robot()
-turn(robot)
-# main()
-# turning_test()
+while 1:
+    main()
+    sleep(10)
