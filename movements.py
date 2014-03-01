e19ebed6ec81ea271b92542b10df1c15ce8f2ff2
Homletmoo <samlhub@gmail.com>
2014-03-01 15:04:30 +0000

    importsss

--- movements.py
+++ movements.py
@@ -4,24 +4,8 @@ from math import sin, cos, sqrt
 from time import sleep
 
 from position import compute_directions_for_point, position_from_zone
+from geometry import Vec2
 
-
-class Vec2:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        
-    def __add__(self, other):
-        return Vec2(self.x+other.x, self.y+other.y)
-    
-    def __sub__(self, other):
-        return Vec2(self.x-other.x, self.y-other.y)
-    
-    def __len__(self):
-        return sqrt(self.x**2 + self.y**2)
-    
-    def dist(self, other):
-        return sqrt((other.x-self.x)**2 + (other.y-self.y)**2)
 
 class Tracker(Vec2):
     def __init__(self, zone_number):
