08ce7c44a72b2b80a173df879f97ebcf2f10330f
Nikita <nikita.karandej@gmail.com>
2014-03-01 10:50:15 +0000

    qq

--- movements.py
+++ movements.py
@@ -2,16 +2,6 @@ from mechanics import Journey
 from position import position_from_zone
 from math import sin, cos
 
-def move_straight(robot, dist):
-    journey = Journey(robot, distance=dist)
-    journey.start()
-    robot.position.move(dist)
-
-def turn(robot, alpha=0.524):  # 0.524 rad = 30 degrees
-    journey = Journey(robot, angle=alpha)
-    journey.start()
-    robot.position.angle += alpha
-
 class Tracker:
     def __init__(self, zone_number):
         position = position_from_zone(zone_number)
@@ -20,8 +10,22 @@ class Tracker:
     def move(self, dist):
         self.x += dist * sin(self.angle)
         self.y += dist * cos(self.angle)
+
+    def turn(self, angle):
+        self.angle += angle
         
     def reset(self, x, y, angle):
         self.x = x
         self.y = y
         self.angle = angle
+
+
+def move_straight(robot, dist):
+    journey = Journey(robot, distance=dist)
+    journey.start()
+    robot.position.move(dist)
+
+def turn(robot, alpha=0.524):  # 0.524 rad = 30 degrees
+    journey = Journey(robot, angle=alpha)
+    journey.start()
+    robot.position.turn(alpha)
