ecc6f0f1e53e1e3355efa9df5911089b16e4d3d5
Homletmoo <samlhub@gmail.com>
2014-03-11 17:17:25 +0000

    can't remember

--- robot.py
+++ robot.py
@@ -9,8 +9,7 @@ from strategy import line_up_to_marker, scan_corner, move_till_touch
 def main():
     robot.position = Tracker(robot.zone)
     p = robot.position
-    print 'Start\n    x = %.1f y = %.1f theta = %.1f' % (p.x, p.y, p.theta)
-    # print '   ', (robot.position.x, robot.position.y), robot.position.angle
+    print 'Start\n    x = %.1f y = %.1f theta = %.1f' % p.x, p.y, p.theta
     
     # Main strategy goes here:
     marker = scan_corner(robot, robot.zone)
@@ -18,8 +17,7 @@ def main():
     move_till_touch(robot)
     
     p = robot.position
-    print 'End\n    x = %.1f y = %.1f theta = %.1f' % (p.x, p.y, p.theta)
-    # print '   ', (robot.position.x, robot.position.y), robot.position.angle
+    print 'End\n    x = %.1f y = %.1f theta = %.1f' % p.x, p.y, p.theta
 
 robot = Robot()
 
@@ -27,17 +25,3 @@ world_exists = True
 while world_exists:
     main()
     sleep(5)
-
-# from time import sleep
-# from sr import Robot
-
-# def main():
-#     robot = Robot()
-#     robot.motors[0].m0.power = 50
-#     robot.motors[0].m1.power = 50
-#     sleep(10)
-#     robot.motors[0].m0.power = 0
-#     robot.motors[0].m1.power = 0
-    
-
-# main()
