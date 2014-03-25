a0800d399a9837fb05beec6d6f4fe1c19b70dfd7
Nikita <nikita.karandej@gmail.com>
2014-03-25 15:36:37 +0000

    approximations

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 from time import sleep
 from traceback import print_exc
+from math import degrees
 
 from sr import Robot
 
@@ -29,13 +30,51 @@ def get_to_marker(robot):
     line_up_to_marker(robot, marker)
     move_till_touch(robot)
 
+def see_marker(robot):
+    markers = robot.see()
+    while not markers:
+        markers = robot.see()
+    return markers[0]
+
+def measure_distance(robot, dist):
+    marker = see_marker(robot)
+    dist0 = marker.dist
+    move_straight(dist)
+    marker = see_marker(robot)
+    dist1 = marker.dist
+    print 'Attempted moving %.1f m;\nMoved %.1f m' % (dist, dist0 - dist1)
+
+def measure_angle(robot, angle):
+    marker = see_marker(robot)
+    alpha = marker.orientation.rot_y
+    turn(robot, angle)
+    marker = see_marker(robot)
+    beta = marker.orientation.rot_y
+    angle = degrees(angle)
+    print 'Attempted turning %.1f deg;\nTurned %.1f deg' % (angle, alpha - beta)
+    
+    
+def approximations(robot):
+    distances = [1, 0.50, 0.25, 0.1, 0.5]
+    angles = [pi/6, pi/12, pi/36]
+    for dist in distances:
+        measure_distance(robot, dist)
+        sleep(10)
+
+    sleep(20)
+
+    for angle in angles:
+        measure_angle(robot, angle)
+        sleep(10)
+
 def main():
     robot = Robot()
     robot.position = Tracker(robot.zone)
     while 1:
         try:
             # get_to_marker()
-            use_arms(robot)
+            # use_arms(robot)
+            approximations()
         except:
             print_exc()
             print '\nRestarting in 2s...\n'
