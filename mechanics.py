72dd4ac6cb21d5e7a039605a4e2739c2d35c6b3f
Homletmoo <samlhub@gmail.com>
2014-03-26 14:22:41 +0000

    so much PEP8

--- mechanics.py
+++ mechanics.py
@@ -7,14 +7,14 @@ from sr import INPUT_PULLUP
 
 NOTCHES_ON_WHEEL = 4
 ROBOT_RADIUS = 0.185
-M_SWITCH_LEFT  = 13
+M_SWITCH_LEFT = 13
 M_SWITCH_RIGHT = 12
 WHEEL_CIRCUMFERENCE = 0.31
 LEFT_ARM = 1
 ARMS_LIFT = 0
 RIGHT_ARM = 2
-ARMS_FORWARDS_STOP = 0 # TODO
-ARMS_BACKWARDS_STOP = 0 # TODO
+ARMS_FORWARDS_STOP = 0  # TODO
+ARMS_BACKWARDS_STOP = 0  # TODO
 
 
 class Journey:
@@ -28,21 +28,25 @@ class Journey:
         self.run = True
         if distance != 0:
             self.length = distance
-            approximation = (40, 8)
+            approx = (40, 8)
         elif angle != 0:
             self.length = -angle * ROBOT_RADIUS
-            approximation = (50, 3.7)
+            approx = (50, 3.7)
         else:
             self.run = False
-            approximation = (0, 0)
+            approx = (0, 0)
             return
 
         turnsToDo = self.length / WHEEL_CIRCUMFERENCE
-      
-        self.m0 = Motor(robot.motors[0].m0, M_SWITCH_LEFT, rduino, turnsToDo, approximation)
+
+        self.m0 = Motor(
+            robot.motors[0].m0, M_SWITCH_LEFT, rduino, turnsToDo, approx
+        )
         if angle != 0:
             turnsToDo *= -1
-        self.m1 = Motor(robot.motors[0].m1, M_SWITCH_RIGHT, rduino, turnsToDo, approximation)
+        self.m1 = Motor(
+            robot.motors[0].m1, M_SWITCH_RIGHT, rduino, turnsToDo, approx
+        )
 
     def start(self):
         if self.run:
@@ -59,12 +63,12 @@ class Motor(Thread):
     Drives a motor a given number of turns using micro-switches or,
     for small distances, approximation. Runs concurrently.
     """
-    def __init__(self, motor, switchID, rduino, turns, approximation):
+    def __init__(self, motor, switchID, rduino, turns, approx):
         super(Motor, self).__init__()
         self.switchID = switchID
         self.motor = motor
         self.turns = turns
-        self.approximation = approximation
+        self.approx = approx
         self.ruggeduino = rduino
         self.ruggeduino.pin_mode(switchID, INPUT_PULLUP)
 
@@ -86,9 +90,9 @@ class Motor(Thread):
     def run(self):
         triggers = abs(self.turns) * NOTCHES_ON_WHEEL
         if triggers < 2:
-            # Probably need different powers for turning/moving forward of the robot
-            self.motor.power = copysign(self.approximation[0], self.turns) # was 50 and time = 3.7
-            sleep(self.approximation[1] * abs(self.turns) * WHEEL_CIRCUMFERENCE)
+            # Probably need different powers for turning/moving forward
+            self.motor.power = copysign(self.approx[0], self.turns) # was 50 and time = 3.7
+            sleep(self.approx[1] * abs(self.turns) * WHEEL_CIRCUMFERENCE)
         else:
             self.motor.power = copysign(50, self.turns)
             total_t = 0
@@ -116,30 +120,35 @@ def open_arms(robot):
     robot.servos[0][LEFT_ARM] = 50
     robot.servos[0][RIGHT_ARM] = 50
 
+
 def close_arms(robot):
     pos = 90
     robot.servos[0][LEFT_ARM] = pos
     robot.servos[0][RIGHT_ARM] = 100 - pos
 
+
 def raise_arms(robot):
     robot.servos[0][ARMS_LIFT] = 0 # TODO
 
+
 def lower_arms(robot):
     robot.servos[0][ARMS_LIFT] = 100 # TODO
+
 
 def init_arms_pins(robot):
     robot.ruggeduinos[0].pin_mode(ARMS_FORWARDS_STOP, INPUT_PULLUP)
     robot.ruggeduinos[0].pin_mode(ARMS_BACKWARDS_STOP, INPUT_PULLUP)
 
+
 def extend_arms(robot, power):
     stop_pin = ARMS_FORWARDS_STOP if power >= 0 else ARMS_BACKWARDS_STOP
-    
+
     hit_stop = False
     beyond_time_limit = False
-    
+
     start = time()
     robot.motors[1].m0.power = power
     while not (hit_stop or beyond_time_limit):
         hit_stop = robot.ruggeduinos[0].digital_read(stop_pin)
-        beyond_time_limit = time() > start + 5 # TODO
+        beyond_time_limit = time() > start + 5  # TODO
     robot.motors[1].m0.power = 0
