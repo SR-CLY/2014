927b6d40c03cf493e5065328fa657f0a6c36ebb7
Homletmoo <samlhub@gmail.com>
2014-03-26 14:48:25 +0000

    push and pop can be applied multiple times from one call

--- logging.py
+++ logging.py
@@ -4,19 +4,20 @@ def log(robot, s):
         robot.log_level = 0
     print "  " * robot.log_level + s
 
-def push_log(robot):
+def push_log(robot, repeat=1):
     """Increases indentation level for output."""
-    if hasattr(robot, "log_level"):
-        if robot.log_level > 0:
-            robot.log_level += 1
-    else:
-        robot.log_level = 1
+    for i in range(repeat):
+        if hasattr(robot, "log_level"):
+            if robot.log_level > 0:
+                robot.log_level += 1
+        else:
+            robot.log_level = 1
 
-def pop_log(robot):
+def pop_log(robot, repeat=1):
     """Decreases indentation level for output."""
-    if hasattr(robot, "log_level"):
-        if robot.log_level > 0:
-            robot.log_level -= 1
-    else:
-        robot.log_level = 0
-    print()
+    for i in range(repeat):
+        if hasattr(robot, "log_level"):
+            if robot.log_level > 0:
+                robot.log_level -= 1
+        else:
+            robot.log_level = 0
