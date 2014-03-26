2cf8df4d50839d21656310f570c61d1eb0f70dc9
Homletmoo <samlhub@gmail.com>
2014-03-26 14:18:24 +0000

    PEP8

--- movements.py
+++ movements.py
@@ -5,23 +5,23 @@ from mechanics import (Journey, open_arms, close_arms, raise_arms, lower_arms,
     init_arms_pins, extend_arms)
 from position import position_from_zone
 
+
 ARMS_POWER = 15
+
 
 class Tracker():
     """
     Tracks the robot's current position and bearing.
-    
     Position is stored as displacement from corner 0: x, y in meters.
-    
     Bearing is stored as theta in radians.
     """
     def __init__(self, zoneNumber):
         self.x, self.y, self.theta = position_from_zone(zoneNumber)
-    
+
     def move(self, dist):
         self.x += dist * sin(self.theta)
         self.y -= dist * cos(self.theta)
-    
+
     def turn(self, angle):
         self.theta += angle
         self.theta %= 2*pi
@@ -35,6 +35,7 @@ def move_straight(robot, dist):
     journey.start()
     robot.position.move(dist)
 
+
 def turn(robot, alpha=0.524):  # 0.524 rad = 30 degrees
     """
     Turns the robot alpha RADIANS and updates the tracker.
@@ -43,12 +44,14 @@ def turn(robot, alpha=0.524):  # 0.524 rad = 30 degrees
     journey.start()
     robot.position.turn(alpha)
 
+
 def prepare_grab(robot):
     init_arms_pins(robot)
     close_arms(robot)
     lower_arms(robot)
     extend_arms(robot, ARMS_POWER)
     open_arms(robot)
+
 
 def grab(robot):
     init_arms_pins(robot)
