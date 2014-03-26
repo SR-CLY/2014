48b6d561e85bf0ed397bbef6edf42694612eab97
Homletmoo <samlhub@gmail.com>
2014-03-26 14:44:54 +0000

    fixed hasattr calls

--- logging.py
+++ logging.py
@@ -1,12 +1,12 @@
 def log(robot, s):
     """Prints message with indentation based on stack."""
-    if not hasattr(robot, log_level):
+    if not hasattr(robot, "log_level"):
         robot.log_level = 0
     print "  " * robot.log_level + s
 
 def push_log(robot):
     """Increases indentation level for output."""
-    if hasattr(robot, log_level):
+    if hasattr(robot, "log_level"):
         if robot.log_level > 0:
             robot.log_level += 1
     else:
@@ -14,7 +14,7 @@ def push_log(robot):
 
 def pop_log(robot):
     """Decreases indentation level for output."""
-    if hasattr(robot, log_level):
+    if hasattr(robot, "log_level"):
         if robot.log_level > 0:
             robot.log_level -= 1
     else:
