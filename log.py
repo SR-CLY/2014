bd51095f057bedee3bc1af6e896420ebd5d835e9
Homletmoo <samlhub@gmail.com>
2014-03-29 15:27:16 +0000

    log fix

--- log.py
+++ log.py
@@ -1,25 +1,21 @@
 def log(robot, s):
     """Prints message with indentation based on stack."""
-#    if not hasattr(robot, "log_level"):
-#        robot.log_level = 0
-    print ("  " * robot.log_level) + s
+    print ("-" * robot.log_level) + s
+
+
+def reset_log(robot):
+    """Resets the log level of the robot."""
+    robot.log_level = 0
 
 
 def push_log(robot, repeat=1):
     """Increases indentation level for output."""
     for i in range(repeat):
-        if hasattr(robot, "log_level"):
-            if robot.log_level > 0:
-                robot.log_level += 1
-        else:
-            robot.log_level = 1
+        robot.log_level += 1
 
 
 def pop_log(robot, repeat=1):
     """Decreases indentation level for output."""
     for i in range(repeat):
-        if hasattr(robot, "log_level"):
-            if robot.log_level > 0:
-                robot.log_level -= 1
-        else:
-            robot.log_level = 0
+        if robot.log_level > 0:
+            robot.log_level -= 1
--- robot.py
+++ robot.py
@@ -10,12 +10,14 @@ from strategy import get_token_from_corner, token_to_slot
 def main():
     robot = Robot()
     robot.position = Tracker(robot.zone)
+    reset_log(robot)
     while 1:
         try:
             get_token_from_corner(robot, robot.zone)
             token_to_slot(robot, robot.zone)
         except:
             print_exc()
+            reset_log(robot)
             print '\nRestarting in 2s...\n'
             sleep(2)
 
