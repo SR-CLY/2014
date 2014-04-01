0713a13d02465226e7863bacee5131c073a89225
Alex Monk <krenair@gmail.com>
2014-04-01 15:16:54 +0100

    resolution tst

--- robot.py
+++ robot.py
@@ -8,6 +8,13 @@ from movements import Tracker, turn
 from strategy import get_token_from_corner, token_to_slot
 
 
+def resolution_test(robot):
+    res = (1280, 960)
+    markers = robot.see(res=res)
+    while not markers:
+        markers = robot.see(res=res)
+    print 'Seen %d markers' % (len(markers))
+
 def main():
     robot = Robot()
     robot.position = Tracker(robot.zone)
@@ -15,8 +22,10 @@ def main():
 
     while 1:
         try:
-            get_token_from_corner(robot, robot.zone)
-            token_to_slot(robot, robot.zone)
+            # get_token_from_corner(robot, robot.zone)
+            # token_to_slot(robot, robot.zone)
+            resolution_test(robot)
+            sleep(5)
         except:
             print_exc()
             reset_log(robot)
