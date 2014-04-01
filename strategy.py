f296c3097ab146d8d70fd94ccc0e1db44ac0e265
Jake Howard <git@theorangeone.net>
2014-04-01 17:33:03 +0100

    remove ghost sleep

--- strategy.py
+++ strategy.py
@@ -231,7 +231,7 @@ def move_till_touch(robot, time_limit=30):  # Experiment with limit default.
     robot.motors[0].m1.power = 0
 
     log(robot, "done.")
-    sleep(2)
+
 
     # Update robot.position with distance moved.
     robot.position.move((time()-start) / 5)
