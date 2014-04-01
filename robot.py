e0777527de238f321bc72c33d0a049a1f2042f7b
Alex Monk <krenair@gmail.com>
2014-04-01 17:11:10 +0100

    test if we actually have a token before going to slot

--- robot.py
+++ robot.py
@@ -6,15 +6,6 @@ from sr import Robot
 from log import reset_log
 from movements import Tracker, turn
 from strategy import get_token_from_corner, token_to_slot
-
-
-def resolution_test(robot):
-    resolutions = [(1280, 960), (800, 600)]
-    for res in resolutions:
-        start = time()
-        markers = robot.see(res=res)
-        print 'took %.1f s to take picture with %s' % (time()-start, res)
-        print 'Seen %d markers' % (len(markers))
         
 
 def main():
@@ -22,15 +13,10 @@ def main():
     robot.position = Tracker(robot.zone)
     reset_log(robot)
 
-    i = 0
     while 1:
         try:
-            get_token_from_corner(robot, robot.zone)
-            token_to_slot(robot, robot.zone)
-            # print i
-            # resolution_test(robot)
-            # i += 1
-            # sleep(0)
+            if get_token_from_corner(robot, robot.zone):
+                token_to_slot(robot, robot.zone)
         except:
             print_exc()
             reset_log(robot)
