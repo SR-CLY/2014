35f80ec2dba5fd9ea6d746ea8cafec7cdeb45633
olls <olls@ovi.com>
2014-04-02 16:59:41 +0100

    fix

--- movements.py
+++ movements.py
@@ -19,7 +19,7 @@ class Tracker():
         self.x, self.y, self.theta = position_from_zone(zoneNumber)
         
     def __repr__(self):
-        return 'Tracker(' + ', '.join([self.x, self.y, self.theta]) + ')'
+        return 'Tracker(' + ', '.join([repr(self.x), repr(self.y), repr(self.theta)]) + ')'
 
     def move(self, dist):
         self.x += dist * sin(self.theta)
