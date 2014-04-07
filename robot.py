3609dc34d05ef38fcac658286e3a0f18af488858
Nikita <nikita.karandej@gmail.com>
2014-04-07 19:28:29 +0100

    Now robot will get 4 markers and try to put them in four slots on one side of the wall(to earn 4 bonus points).

--- robot.py
+++ robot.py
@@ -6,30 +6,44 @@ from log import log, reset_log
 from movements import Tracker, turn
 from strategy import get_token_from_corner, token_to_slot, recalulate_position
 
+
 def main():
     robot = Robot()
     robot.position = Tracker(robot.zone)
     reset_log(robot)
-    
-    while True:
-        recalulate_position(robot)
-        print(robot.position)
-        sleep(5)
 
-    # while 1:
-    #     try:
-    #         token_to_slot(robot, robot.zone)
-    #         i = 0
-    #         while i < 4:
-    #             zone = (robot.zone + i) % 4
-    #             if get_token_from_corner(robot, zone):
-    #                 token_to_slot(robot, zone)
-    #             i += 1
-    #     except:
-    #         print_exc()
-    #         reset_log(robot)
-    #         print '\nRestarting in 2s...\n'
-    #         sleep(2)
+    slots_x = 3 if robot.zone in [0, 3] else 5.18
+    target_theta = pi/2 if robot.zone in [0, 3] else 1.5*pi
+    if robot.zone in [0, 1]:
+        dy = 0.9
+        slot_y_0 = 2.65
+    else:
+        dy = -0.9
+        slot_y_0 = 5.65
+
+    while 1:
+        try:
+            for i in range(4):
+                if i == 1:
+                    get_token_from_corner(robot, robot.zone)
+                elif i > 1:
+                    if robot.zone == 0:
+                        get_token_from_corner(robot, 3)
+                    if robot.zone == 1:
+                        get_token_from_corner(robot, 2)
+                    if robot.zone == 2:
+                        get_token_from_corner(robot, 1)
+                    if robot.zone == 3:
+                        get_token_from_corner(robot, 0)
+
+                slot_y = slot_y_0 + i*dy
+                move_to_point(robot, slots_x, slot_y, target_theta)
+                token_to_slot(robot)
+        except:
+            print_exc()
+            reset_log(robot)
+            print '\nRestarting in 2s...\n'
+            sleep(2)
 
 
 main()
