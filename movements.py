9ef77a353c677a594fdee6fa9d2afee92040d4da
Nikita <nikita.karandej@gmail.com>
2014-04-08 19:12:20 +0100

    Moved Tracker class to a separate file.

--- movements.py
+++ movements.py
@@ -1,4 +1,3 @@
-from math import sin, cos, pi
 from time import sleep
 
 from mechanics import (init_arms_pins, close_arms, lower_arms, extend_arms,
@@ -7,28 +6,6 @@ from position import position_from_zone
 
 
 ARMS_POWER = 15
-
-
-class Tracker():
-    """
-    Tracks the robot's current position and bearing.
-    Position is stored as displacement from corner 0: x, y in meters.
-    Bearing is stored as theta in radians.
-    """
-    def __init__(self, zoneNumber):
-        self.x, self.y, self.theta = position_from_zone(zoneNumber)
-        
-    def __repr__(self):
-        return 'Tracker(' + ', '.join([repr(self.x), repr(self.y), repr(self.theta)]) + ')'
-
-    def move(self, dist):
-        self.x += dist * sin(self.theta)
-        self.y -= dist * cos(self.theta)
-
-    def turn(self, angle):
-        self.theta += angle
-        self.theta %= 2*pi
-
 
 def move_straight(robot, dist):
     """
