6cf3371cd19ccfd7c517c128bfbaae5868bd2930
Alex Monk <krenair@gmail.com>
2014-04-09 12:00:37 +0100

    add log

--- mechanics.py
+++ mechanics.py
@@ -157,6 +157,7 @@ def extend_arms(robot, power):
     robot.motors[1].m1.power = power
     while not (hit_stop or beyond_time_limit):
         hit_stop = robot.ruggeduinos[0].digital_read(stop_pin)
+        log(robot, hit_stop)
         beyond_time_limit = time() > start + 10
     robot.motors[1].m1.power = 0
     log(robot, "Stopping arms.")
