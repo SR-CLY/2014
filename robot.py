9743485c8229afa148f239a04d56dae698fcffcb
Homletmoo <samlhub@gmail.com>
2014-04-26 11:23:56 +0100

    abc test

--- robot.py
+++ robot.py
@@ -7,7 +7,7 @@ from sr import Robot, INPUT_PULLUP
 from log import reset_log, log
 from tracker import Tracker
 from sound import Sound
-from abc import ABC
+import ABC
 from strategy import (get_token_from_corner, token_to_slot, move_to_point,
     move_till_touch, FRONT_SWITCH)
 from movements import put_down, grab, move_straight, turn
@@ -30,7 +30,7 @@ def main():
     robot.position = Tracker(robot.zone)
     set_pins(robot)
 
-    dixie = ABC("dixie.abc")
+    dixie = abc.ABC("dixie.abc")
     dixie.play(robot, 1)
 
     approx(robot)
